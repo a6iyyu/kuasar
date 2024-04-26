e9e66bdf7abd4f81c0a8b25709d2923cb6032694
ffst4irs@gmail.com <ffst4irs@gmail.com>
2024-04-26 13:11:03 +0700

    First initialization.

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,tsx,ts,mts,cts,cjs,mjs,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
