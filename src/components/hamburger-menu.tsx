8399b8b953e82fa995fef2fda0c61f16117d4a68
ffst4irs@gmail.com <ffst4irs@gmail.com>
2024-04-28 21:39:09 +0700

    Finish.

--- src/components/hamburger-menu.tsx
+++ src/components/hamburger-menu.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const HamburgerMenu: React.FC = () => {
   const [open, setOpen] = useState<boolean>(false);
+  const menus: string[] = ["About", "Summary", "Search"];
 
   return (
     <section className="flex h-full w-3/5 items-center justify-end lg:hidden">
@@ -14,15 +15,15 @@ const HamburgerMenu: React.FC = () => {
           className="absolute top-24 h-fit w-fit rounded-xl bg-slate-50"
           data-aos={open ? "fade-down" : "fade-up"}
         >
-          <h5 className="grid h-full w-full cursor-pointer place-items-center rounded-xl py-5 font-semibold text-slate-950 transition-all duration-300 ease-in-out hover:bg-slate-200">
-            About
-          </h5>
-          <h5 className="grid h-full w-full cursor-pointer place-items-center rounded-xl py-5 font-semibold text-slate-950 transition-all duration-300 ease-in-out hover:bg-slate-200">
-            Summary
-          </h5>
-          <h5 className="grid h-full w-full cursor-pointer place-items-center rounded-xl py-5 font-semibold text-slate-950 transition-all duration-300 ease-in-out hover:bg-slate-200">
-            Search
-          </h5>
+          {menus.map((menu, i) => (
+            <h5
+              id={menu.toLowerCase()}
+              key={i}
+              className="grid h-full w-full cursor-pointer place-items-center rounded-xl py-5 font-semibold text-slate-950 transition-all duration-300 ease-in-out hover:bg-slate-200"
+            >
+              {menu}
+            </h5>
+          ))}
         </div>
       )}
     </section>
