3b66c8156335e7309a619f1194c734b2b45d6534
ffst4irs@gmail.com <ffst4irs@gmail.com>
2024-04-28 23:07:00 +0700

    Finish.

--- src/components/hamburger-menu.tsx
+++ src/components/hamburger-menu.tsx
@@ -1,24 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const HamburgerMenu: React.FC = () => {
   const [open, setOpen] = useState<boolean>(false);
   const menus: string[] = ["About", "Summary", "Search"];
 
+  const HandleMenulick = (target: string) => {
+    if (document.querySelector(`.${target}-section`)) {
+      document.querySelector(`.${target}-section`)?.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  };
+
+  useEffect(() => {
+    const HandleOutside = (e: MouseEvent) => {
+      if (!(e.target as HTMLElement).closest(".hamburger-menu")) {
+        setOpen(false);
+      }
+    };
+
+    document.body.addEventListener("click", HandleOutside);
+    return () => document.body.removeEventListener("click", HandleOutside);
+  }, [open]);
+
   return (
-    <section className="flex h-full w-3/5 items-center justify-end lg:hidden">
+    <section className="hamburger-menu flex h-full w-3/5 items-center justify-end lg:hidden">
       <i
         className="fa-solid fa-bars cursor-pointer text-3xl text-gray-50"
         onClick={() => setOpen(!open)}
       ></i>
       {open && (
         <div
-          className="absolute top-24 h-fit w-fit rounded-xl bg-slate-50"
+          className="absolute top-24 z-50 h-fit w-fit rounded-xl bg-slate-50"
           data-aos={open ? "fade-down" : "fade-up"}
         >
           {menus.map((menu, i) => (
             <h5
               id={menu.toLowerCase()}
               key={i}
+              onClick={() => HandleMenulick(menu.toLowerCase())}
               className="grid h-full w-full cursor-pointer place-items-center rounded-xl py-5 font-semibold text-slate-950 transition-all duration-300 ease-in-out hover:bg-slate-200"
             >
               {menu}
